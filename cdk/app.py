6eb7860de1a62248be66dcdffaee733b1587e3a5
aws-kh <aws-kh@users.noreply.github.com>
2021-07-08 15:49:08 +0000

    WIP: updated app.py to include stack cli context

--- cdk/app.py
+++ cdk/app.py
@@ -1,9 +1,37 @@
 #!/usr/bin/env python3
+"""
+
+$ cdk deploy --context \
+    stack_name=LambdaMapStack \
+    function_name=LambdaMapFunction \
+    memory_size=512 \
+    timeout_secs=900
+
+"""
 from aws_cdk import core as cdk
 from stack.stack import LambdaMapStack
 
 
 if __name__ == '__main__':
     app = cdk.App()
-    LambdaMapStack(app, "LambdaMapStack")
+
+    stack_name = app.node.try_get_context("stack_name")
+    function_name = app.node.try_get_context("function_name")
+    memory_size = app.node.try_get_context("memory_size")
+    timeout_secs = app.node.try_get_context("timeout_secs")
+
+    if stack_name is None:
+        stack_name = "LambdaMapStack"
+    if function_name is None:
+        function_name = "LambdaMapFunction"
+    if memory_size is None:
+        memory_size = 512
+    if timeout_secs is None:
+        timeout_secs = 900
+
+    kwargs = dict(stack_name=stack_name, function_name=function_name,
+                  memory_size=memory_size, timeout_secs=timeout_secs)
+
+    LambdaMapStack(app, stack_name, **kwargs)
+
     app.synth()
--- cdk/stack/stack.py
+++ cdk/stack/stack.py
@@ -3,24 +3,44 @@ import os
 from aws_cdk import core, aws_lambda, aws_ecr
 from aws_cdk import core as cdk
 
-PWD = os.path.dirname(os.path.realpath(__file__))
-
 
 class LambdaMapStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         """
-        """
-        super().__init__(scope, construct_id, **kwargs)
 
-        ecr_image = aws_lambda.EcrImageCode.from_asset_image(directory=PWD)
+        Parameters
+        ----------
+        folder : str, optional
+            /path/to the folder containing the lambda function .py file
+            (i.e. lambda.py) and Dockerfile.
+        function_name : str, default="LambdaMapFunction"
+            Name of the primary lambdamap function.
+        memory_size : int, default=512
+            Amount of memory allocated (in MB) to the lambdamap function.
+        timeout_secs : int, default=900
+            Max. amount of time (in seconds) the lambdamap function will run
+            before stopping.
+
+        """
+        super().__init__(scope, construct_id)
+
+        folder = \
+            kwargs.get("folder", os.path.dirname(os.path.realpath(__file__)))
+        function_name = kwargs.get("function_name", "LambdaMapFunction")
+        memory_size = kwargs.get("memory_size", 512) 
+        timeout_secs = kwargs.get("timeout_secs", 900) 
+
+        ecr_image = aws_lambda.EcrImageCode.from_asset_image(directory=folder)
 
         lambda_function = aws_lambda.Function(self, 
-          id            = "LambdaMapFunction",
+          id            = function_name,
           description   = "LambdaMap",
           code          = ecr_image,
           handler       = aws_lambda.Handler.FROM_IMAGE,
           runtime       = aws_lambda.Runtime.FROM_IMAGE,
-          function_name = "LambdaMapFunction",
-          memory_size   = 512,
-          timeout       = core.Duration.minutes(15),
+          function_name = function_name,
+          memory_size   = memory_size,
+          timeout       = core.Duration.seconds(timeout_secs),
         )
+
+        return
